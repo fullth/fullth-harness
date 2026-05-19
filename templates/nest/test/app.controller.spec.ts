4c4c1e1f6013466a0454690ea5aa96b9abfde103
fullth <xoghksdla@gmail.com>
2026-05-19 16:23:07 +0900

    NestJS 보일러플레이트 템플릿 추가

---
+++ templates/nest/test/app.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+
+describe('AppController', () => {
+  let controller: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+  });
+
+  it('health 는 status ok 를 반환한다', () => {
+    expect(controller.getHealth()).toEqual({ status: 'ok' });
+  });
+});
